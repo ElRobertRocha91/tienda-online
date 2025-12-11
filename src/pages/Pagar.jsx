16dbf634d40111c52d26fa6dc904936dd741fd5a
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2025-12-11 10:31:50 -0300

    Agregando Estilos Responsivos - Tabla de Productos en Pagar

--- src/pages/Pagar.jsx
+++ src/pages/Pagar.jsx
@@ -1,10 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../context/AuthContext";
 import { useCartContext } from "../context/CartContext";
+import styles from "../styles/pages/Pagar.module.css";
 
 function Pagar() {
   const { usuario, cerrarSesion } = useAuthContext();
-  const { carrito, total, vaciarCarrito } = useCartContext();
+  const { carrito, total, vaciarCarrito, agregarCantidad, quitarCantidad } = useCartContext();
   const navigate = useNavigate();
 
   // Obtengo el Token-Usuario
@@ -18,32 +19,67 @@ function Pagar() {
   };
 
   return (
-    <div>
-      <div>
-        <h2>{usuario.nombre}</h2>
+    <div className={styles.pagePagar}>
+      <div className={styles.datoCliente}>
+        <h2>Cliente: {usuario.password}</h2>
         <p>Email: {usuario.email}</p>
-        <strong>Token: <code>{tokenActual}</code></strong>
-        <button onClick={cerrarSesion}>Cerrar sesión</button>
-        <hr />
+        {/* <strong>Token: <code>{tokenActual}</code></strong>
+        <button onClick={cerrarSesion}>Cerrar sesión</button> */}
       </div>
-
+      <hr />
       <div>
         <h2>Tu compra:</h2>
-
-        {carrito.map((producto) => (
-          <div key={producto.id}>
-            <img src={producto.avatar} alt={producto.nombre} width="60" />
-            <span>{producto.nombre}</span>
-            <strong>${producto.precio}</strong>
-          </div>
-        ))}
-
-        <h3>Total a pagar: ${total}</h3>
+        {carrito.length === 0 ? (
+          <p>El carrito esta vacío</p>
+        ) : (
+          <table className={styles.tablaCompra}>
+            <thead>
+              <tr>
+                <th>Imagen</th>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {carrito.map((producto) => (
+                <tr key={producto.id}>
+                  <td className={styles.colImagen}>
+                    <img
+                      src={producto.avatar}
+                      alt={producto.nombre}
+                      width="120"
+                      height="100"
+                    />
+                  </td>
+                  <td>
+                    <span>{producto.nombre}</span>
+                  </td>
+                  <td>
+                    <strong>${producto.precio}</strong>
+                  </td>
+                  <td>
+                    Cantidad: {producto.cantidad || 1}
+                  </td>
+                  <td>
+                    <button onClick={() => quitarCantidad(producto.id)} className={styles.btn}>-</button>
+                    <button onClick={() => agregarCantidad(producto.id)} className={styles.btn}>+</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+        <div>
+          <h3 className={styles.pagar}>Total a pagar: ${total.toFixed(3)}</h3>
+        </div>
       </div>
 
-      <div>
-        <button onClick={comprar}>Confirmar y Pagar</button>
-        <button onClick={() => navigate("/productos")}>Cancelar</button>
+      <div className={styles.listBtn}>
+        <button onClick={comprar} className={styles.celeste}>Confirmar y Pagar</button>
+        <button onClick={vaciarCarrito} className={styles.rojo}>Vaciar Carrito</button>
+        <button onClick={() => navigate("/productos")} className={styles.verde}>Seguir Comprando</button>
       </div>
     </div>
   );
