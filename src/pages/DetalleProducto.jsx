107cb02f9873cda1775e0c9284e74966fd1f197f
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2026-04-04 12:25:19 -0300

    Features(details-product): Add new styles responsive

--- src/pages/DetalleProducto.jsx
+++ src/pages/DetalleProducto.jsx
@@ -20,14 +20,14 @@ function DetalleProducto() {
             <section id="detalle">
                 <div className={styles.cajaDetalle}>
                     <div className={styles.tarjetaDetalle}>
-                        <h2>Detalle del Producto {id}</h2>
+                        <h2 className={styles.tituloDetalle}>Detalle del Producto</h2>
                         <li key={producto.id}>
                             <div className={styles.flexDetalle}>
                                 <img src={producto.avatar} alt={producto.nombre} className={styles.imageDetalle} />
                                 <div className={styles.descripcionDetalle}>
                                     <h3 className={styles.nombreDetalle}>{producto.nombre}</h3>
                                     <p><strong>Descripción: </strong>{producto.descripcion}</p>
-                                    <p className={styles.precioDetalle}>Precio: ${producto.precio}</p>
+                                    <p className={styles.precioDetalle}>${producto.precio}</p>
                                 </div>
                             </div>
                             <Link to={'/productos'} className={styles.detalleLink}>Volver</Link>
